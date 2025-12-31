636e686f96373d3e9186f056ff79339f7b37a38e
Colin Skinner <colinaskinner@gmail.com>
2025-12-30 21:39:23 -0800

    Ready for testing (I think)

--- src/core/device-drivers/icm20948.cpp
+++ src/core/device-drivers/icm20948.cpp
@@ -9,11 +9,96 @@ namespace Cesium::Sensor {
 // }
 
 Icm20948::Icm20948(Spi spi, uint8_t cs_pin) 
-    : _spi{spi}, _cs_pin{cs_pin} {
-        Gpio::init_digital(_cs_pin, GpioType::DIGITAL_OUT);
+    : _spi{spi}, _cs_pin{cs_pin}, accel_range{-1}, gyro_range{-1} {
+    Gpio::init_digital(_cs_pin, GpioType::DIGITAL_OUT);
+}
+
+
+uint8_t Icm20948::chip_id() {
+    _select_user_bank(0);
+    return _read_single(REG_WHO_AM_I_BANK_0);
+}
+
+
+bool Icm20948::set_accel_range(uint8_t range)
+{
+    _select_user_bank(2);
+    uint8_t current_value = _read_single(REG_ACCEL_CONFIG_BANK_2);
+
+    uint8_t new_value = (current_value & 0b11111001) | ( (range & 0b11) << 1);
+    _write_single(REG_ACCEL_CONFIG_BANK_2, new_value);
+    
+    current_value = _read_single(REG_ACCEL_CONFIG_BANK_2);
+
+    if (new_value != current_value) {
+        return false;
     }
 
+    return true;
+}
+
+
+bool Icm20948::set_gyro_range(uint8_t range)
+{
+    // This is technically basically a copy of the accel one, oops
+    _select_user_bank(2);
+    uint8_t current_value = _read_single(REG_GYRO_CONFIG_BANK_2);
+
+    uint8_t new_value = (current_value & 0b11111001) | ( (range & 0b11) << 1);
+    _write_single(REG_GYRO_CONFIG_BANK_2, new_value);
     
+    current_value = _read_single(REG_GYRO_CONFIG_BANK_2);
+
+    if (new_value != current_value) {
+        return false;
+    }
+
+    return true;
+}
+
+
+icm20948_data_t Icm20948::read(uint8_t range)
+{
+
+    // If accel range not known already
+    if (accel_range == -1) {
+        _select_user_bank(2);
+        // 0b00000110 
+        accel_range = ((_read_single(REG_ACCEL_CONFIG_BANK_2)) >> 1) & 0b11;
+    }
+    
+    // If gyro range not known already
+    if (gyro_range == -1) {
+        _select_user_bank(2);
+        // 0b00000110 
+        gyro_range = ((_read_single(REG_GYRO_CONFIG_BANK_2)) >> 1) & 0b11;
+    }
+
+    _select_user_bank(0);
+
+    icm20948_data_t result;
+
+    _read_burst(REG_ACCEL_XOUT_H_BANK_0, result.bytes, 14);
+
+    // Scale
+    float accel_scale_factor = ACCEL_RANGE_TO_SCALE_FACTOR.at(accel_range);
+    result.accel_x / accel_scale_factor;
+    result.accel_x / accel_scale_factor;
+    result.accel_x / accel_scale_factor;
+
+    float gyro_scale_factor = GYRO_RANGE_TO_SCALE_FACTOR.at(gyro_range);
+    result.gyro_x / gyro_scale_factor;
+    result.gyro_x / gyro_scale_factor;
+    result.gyro_x / gyro_scale_factor;
+
+    return result;
+}
+
+
+//////////////////////////////////////////////////
+//              Read
+//////////////////////////////////////////////////
+
 uint8_t Icm20948::_read_single(uint8_t reg) {
     
     Gpio::write_digital(_cs_pin, true);
@@ -43,6 +128,10 @@ void Icm20948::_read_burst(uint8_t reg, uint8_t* buffer, uint8_t len) {
     Gpio::write_digital(_cs_pin, false);
 }
 
+//////////////////////////////////////////////////
+//              Write
+//////////////////////////////////////////////////
+
 
 uint8_t Icm20948::_write_single(uint8_t reg, uint8_t val) {
 
@@ -71,9 +160,9 @@ void Icm20948::_write_burst(uint8_t reg, const uint8_t* buffer, uint8_t len) {
     Gpio::write_digital(_cs_pin, false); 
 }
 
+
 void Icm20948::_select_user_bank(uint8_t bank) {
-    bank = bank << 4;
-    _write_burst(REG_BANK_SEL, &bank, 1);
+    _write_single(REG_BANK_SEL, bank << 4);
 }
 
 
--- src/core/device-drivers/icm20948.h
+++ src/core/device-drivers/icm20948.h
@@ -1,28 +1,67 @@
 #pragma once
 
 #include <stdint.h>
+#include <etl/map.h>
 
 #include "core/isolation-layer/peripherals/spi.h"
 #include "core/isolation-layer/peripherals/gpio.h"
 
+/**
+ * TODO:
+ * - Interrupts
+ * - FIFO
+ * - Self-test
+ */
 
 namespace Cesium::Sensor {
 
-
-
+struct icm20948_data_t {
+    // Modeled sort of after the Adafruit method 
+    union {
+        struct {
+            float accel_x; // m/s2
+            float accel_y; // m/s2
+            float accel_z; // m/s2
+            float gyro_x; // dps
+            float gyro_y; // dps
+            float gyro_z; // dps
+            float temp; // deg C
+        };
+        uint8_t bytes[14];
+    };
+};
 
 class Icm20948 {
 
-    // Registers
-    static const uint8_t REG_WHO_AM_I_BANK_1 = 0x7F;
-    static const uint8_t REG_BANK_SEL = 0x7F;
-    
   public:
+
+    // Registers
+    static const uint8_t REG_WHO_AM_I_BANK_0 = 0x00;
+    static const uint8_t REG_ACCEL_XOUT_H_BANK_0 = 0x2D;
+
+    static const uint8_t REG_GYRO_CONFIG_BANK_2 = 0x01;
+    static const uint8_t REG_ACCEL_CONFIG_BANK_2 = 0x14;
+
+    static const uint8_t REG_BANK_SEL = 0x7F;
+
+    static const uint8_t ACCEL_RANGE_2_G = 0;
+    static const uint8_t ACCEL_RANGE_4_G = 1;
+    static const uint8_t ACCEL_RANGE_8_G = 2;
+    static const uint8_t ACCEL_RANGE_16_G = 3;
+
+    static const uint8_t GYRO_RANGE_250_DPS = 0;
+    static const uint8_t GYRO_RANGE_500_DPS = 1;
+    static const uint8_t GYRO_RANGE_1000_DPS = 2;
+    static const uint8_t GYRO_RANGE_2000_DPS = 3;
+
     Icm20948(Spi spi, uint8_t cs_pin);
     // Icm20948(SpiPort spi_port, uint8_t cs_pin, SpiSettings settings);
 
     /* User functions */
     uint8_t chip_id();
+    bool set_accel_range(uint8_t range);
+    bool set_gyro_range(uint8_t range);
+    icm20948_data_t read(uint8_t range);
 
     /* Basic functions */
     void _select_user_bank(uint8_t bank);
@@ -31,9 +70,26 @@ class Icm20948 {
     uint8_t _write_single(uint8_t reg, uint8_t val);
     void _write_burst(uint8_t reg, const uint8_t* buffer, uint8_t len);
 
+
   protected:
     Spi _spi;
     uint8_t _cs_pin;
+    int8_t accel_range;
+    int8_t gyro_range;
+
+    const etl::map<uint8_t, float, 4> ACCEL_RANGE_TO_SCALE_FACTOR = {
+        {ACCEL_RANGE_2_G, 16384.0},
+        {ACCEL_RANGE_4_G, 8192.0},
+        {ACCEL_RANGE_8_G, 4096.0},
+        {ACCEL_RANGE_16_G, 2048.0},
+    };
+
+    const etl::map<uint8_t, float, 4> GYRO_RANGE_TO_SCALE_FACTOR = {
+        {GYRO_RANGE_250_DPS, 16384.0},
+        {GYRO_RANGE_500_DPS, 8192.0},
+        {GYRO_RANGE_1000_DPS, 4096.0},
+        {GYRO_RANGE_2000_DPS, 2048.0},
+    };
 };
 
 
