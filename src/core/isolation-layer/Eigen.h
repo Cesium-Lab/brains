5ea8e0cb0f7a9cc1a7f5e93e81ef49df75bff4ea
Colin Skinner <colinaskinner@gmail.com>
2026-01-02 23:24:45 -0800

    Finally added Quats

--- src/core/isolation-layer/Eigen.h
+++ src/core/isolation-layer/Eigen.h
@@ -4,6 +4,7 @@
 
 namespace Cesium {
     using Vector3f = Eigen::Vector3f;
+    using Quaternionf = Eigen::Quaternionf;
     using Matrix3f = Eigen::Matrix3f;
     using Matrix6f = Eigen::Matrix<float,6,6>;
 }
